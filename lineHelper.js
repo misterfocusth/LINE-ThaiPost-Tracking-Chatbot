edb81b64f2e28754268248b2ca7a785711c17460
Sila Pakdeewong <FocusPakdeewong2546@gmail.com>
2021-08-15 23:30:24 +0700

    Update lineHelper.js

--- lineHelper.js
+++ lineHelper.js
@@ -1,7 +1,7 @@
 const axios = require("axios").default;
 
-const channelAccessToken = "J9y4dPef00T7SdpJzxJHf8SKgYIuqT+JWGreDWw+y3Y5dR6WKQ5u7pqaGGtOJEOEdbAqRMN+/t0gQjjPx7RNi9C8TVXxjGzvD4bfh0mT0gIP85X5Le3toH0gNnU7ZD2OLkjTWUe2U7qtFTDCKTye+AdB04t89/1O/w1cDnyilFU="
-const channelSecret = "d00d4d478d004ef80f24583787e464c6";
+const channelAccessToken = process.env.CAT;
+const channelSecret = process.env.CS;
 
 class lineHelper {
     async reply(replyToken, payload) {
